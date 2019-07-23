fd3618771d244e2bdc796cf6e93488519e18ae46
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2019-07-23 14:51:33 +0200

    working on adversarial training

--- mimic_adversarial/nn.py
+++ mimic_adversarial/nn.py
@@ -3,6 +3,7 @@ import tensorflow.keras.backend as K
 
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
+from tensorflow.keras.optimizers import *
 
 
 def wasserstein_loss(y_true, y_pred):
@@ -59,13 +60,52 @@ def discriminator(length, bins):
     return model
 
 
-def adversarial(length):
-    gen  = generator(length)
-    disc = discriminator(length, 512)
+class TrainGAN:
+    '''
+    Train a Generative Adversarial Neural Network
+    '''    
 
-    return gen, disc
+    def __init__(self, length):
+        self.gen  = generator(length)
+        self.disc = discriminator(length, 512)
+        self.length = length
+        self.adversarial()
 
+    def set_trainable(self, m, val):
+        m.trainable = val
+        for l in m.layers:
+            l.trainable = val
 
-gen, disc = adversarial(100)
-gen.summary()
-disc.summary()
+    def adversarial(self):
+        self.disc.compile(optimizer = RMSprop(lr=0.00005), loss = wasserstein_loss)
+        self.set_trainable(self.disc, False)
+        self.model_in  = Input(shape = (self.length))
+        self.model_out = self.disc(self.gen(self.model_in))
+        self.model     = Model(self.model_in, self.model_out) 
+        self.model.compile(optimizer = RMSprop(lr=0.00005), loss = wasserstein_loss)
+        self.set_trainable(self.disc, True)
+        self.disc.trainable = True        
+        self.disc.summary()
+        self.model.summary()
+        
+    def one_step_generator(self, trace):
+        '''
+        One step of training for the generator.
+        We pretend that fakes are real
+        
+        trace: a whistles trace
+        '''
+        assert(self.length == trace.length)
+        y = np.ones((1,1))
+        x = np.random.uniform(size = (2 * self.length, 1))
+        x[0:self.length, :] = trace
+        return self.model.train_on_batch(x, y)
+    
+    def one_step_disc(self, traces, reals, clip_threshold):
+        valid = np.ones((1,1))
+        fake = -np.ones((1,1))
+        fakes = self.generator.predict(traces)
+        
+        
+        
+TrainGAN(100)
