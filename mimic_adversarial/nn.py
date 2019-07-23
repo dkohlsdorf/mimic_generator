1a503b614137823d9506172f33af44fe06ca3957
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2019-07-23 19:03:04 +0200

    first version of complete gan training

--- mimic_adversarial/nn.py
+++ mimic_adversarial/nn.py
@@ -71,6 +71,12 @@ class TrainGAN:
         self.length = length
         self.adversarial()
 
+    def expand_trace(self, trace):
+        assert trace.shape[0] == self.length
+        expanded = np.random.uniform(size = 2 * self.length)
+        expanded[0:self.length] = trace
+        return expanded
+        
     def set_trainable(self, m, val):
         m.trainable = val
         for l in m.layers:
@@ -84,28 +90,46 @@ class TrainGAN:
         self.model     = Model(self.model_in, self.model_out) 
         self.model.compile(optimizer = RMSprop(lr=0.00005), loss = wasserstein_loss)
         self.set_trainable(self.disc, True)
-        self.disc.trainable = True        
         self.disc.summary()
         self.model.summary()
         
-    def one_step_generator(self, trace):
-        '''
-        One step of training for the generator.
-        We pretend that fakes are real
-        
-        trace: a whistles trace
-        '''
-        assert(self.length == trace.length)
-        y = np.ones((1,1))
-        x = np.random.uniform(size = (2 * self.length, 1))
-        x[0:self.length, :] = trace
-        return self.model.train_on_batch(x, y)
-    
-    def one_step_disc(self, traces, reals, clip_threshold):
-        valid = np.ones((1,1))
-        fake = -np.ones((1,1))
-        fakes = self.generator.predict(traces)
-        
-        
-        
-TrainGAN(100)
+    def one_step_disc(self, trace, real, clip_th):        
+        valid_label = np.ones((1,1))
+        fake_label  = -np.ones((1,1))        
+        fake        = self.generator.predict(self.expand_trace(trace))
+                        
+        d_loss_real = self.disc.train_on_batch(real, valid_label)
+        d_loss_fake = self.disc.train_on_batch(fake, fake_label)
+        d_loss      = 0.5 * (d_loss_real + d_loss_fake)
+
+        for layer in self.disc.layers:
+            weights = layer.get_weigths()
+            weights = [np.clip(w, -clip_th, clip_th) for w in weights]
+            layer.set_weights(weights)        
+        return d_loss, d_loss_real, d_loss_fake
+
+    def one_step_gen(self, trace):
+        valid = np.ones((1,1))        
+        return self.model.train_on_batch(self.expand_trace(trace), valid)                            
+    def train(self, traces, spectrograms, epochs):
+        assert len(traces) == len(spectrograms)
+        n = len(traces)
+        for epoch in range(epochs):
+            d_loss, d_loss_real, d_loss_fake = (0.0, 0.0, 0.0)
+            for i in range(n):
+                d, r, f = self.one_step_disc(
+                    traces[i], spectrograms[i], 0.01
+                )
+                d_loss      += d
+                d_loss_real += r
+                d_loss_fake += f
+
+            g_loss = 0.0
+            for i in range(n):
+               g = one_step_gen(traces[i])
+               g_loss += g
+            print("Discriminator [{} {} {}] || Generator {}".format(
+                d_loss, d_loss_real, d_loss_fake, g_loss
+            ))
+
+            
