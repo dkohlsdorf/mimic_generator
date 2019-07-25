bb97aef0af06b6f6101af4bfb554f634228a986c
Daniel Kohlsdorf <daniel.kohlsdorf@xing.com>
2019-07-25 19:25:40 +0200

    cleaning training script

--- train_gan.py
+++ train_gan.py
@@ -6,18 +6,50 @@ from mimic_utils.data_utils import *
 from mimic_utils.params import *
 from scipy.io import wavfile
 
-whistle_traces  = traces("whistles", 1, 1000)
-whistle_targets = target_spectrograms("target_output", whistle_traces.keys(), 1000)
-data_trace, data_spec = create_dataset(whistle_traces, whistle_targets)
+def header():
+    return """
+    Mimic Geberation Scripts using Generative Adversarial Neural Netorks 
 
-gan = GAN(length = 1000)
-gan.train(data_trace, data_spec, 1)
-gan.save(".")
+    Usage:
+         python train_gan.py WHISTLE_FOLDER TARGET_FOLDER OUTFOLDER LEN EPOCH GEN
 
-for basename, trace in whistle_traces.items():
-    for i in range(10):
-        audio       = gan.create(trace, FFT_WIN, FFT_STEP)
-        outfile     = "output/{}_gan_{}.wav".format(basename, i)
-        outfile_txt = "output/{}_gan_{}.txt".format(basename, i)
-        wavfile.write(outfile, 190000, audio * 10)
-        np.savetxt(outfile_txt, audio)
+    What Will Happen:
+         For each file in the WHISTLE_FOLDER we search associated outputs
+         in the TARGET_FOLDER and train a GAN from whistle traces + noise 
+         to the targets. The results are written in the OUTFOLDER. We padd / cut
+         all traces and spectrogram to the desired LEN. We train the net for
+         EPOCHs and then generate GEN examples for each trace.
+         
+    by Daniel Kohlsdorf
+    """
+
+def params():
+    if len(argv) != 7:
+        return None
+    else:
+        infolder      = argv[1]
+        targetfolder  = argv[2]
+        outfolder     = argv[3]
+        length        = int(argv[4])
+        epochs        = int(argv[5])
+        n_mimics      = int(argv[6])
+        return infolder, targetfolder, outfolder, length, epochs, n_mimics
+
+
+if __name__ == '__main__':
+    print(header())
+    p = params()
+    if p != None:
+        infolder, targetfolder, outfolder, n, epochs, n_mimics = p
+        whistle_traces  = traces(infolder, -1, n)
+        whistle_targets = target_spectrograms(targetfolder, whistle_traces.keys(), n)
+        data_trace, data_spec = create_dataset(whistle_traces, whistle_targets)
+
+        gan = GAN(length = n)
+        gan.train(data_trace, data_spec, epochs)
+        
+        for basename, trace in whistle_traces.items():
+            for i in range(n_mimics):
+                audio       = gan.create(trace, FFT_WIN, FFT_STEP)
+                outfile     = "{}/{}_gan_{}.wav".format(outfolder, basename, i)
+                wavfile.write(outfile, 190000, audio * 10)
